bdffc0b6ff9e27786f133f9c8737de5898186118
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-08-30 16:01:33 +0100

    update dotfiles and add nvim config
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

---
+++ nvim/README.md
@@ -0,0 +1,17 @@
+# 4248vim
+
+My personal [lazy.vim](https://www.lazyvim.org/plugins) configuration.
+
+## Installation
+
+```bash
+mv ~/.config/nvim ~/.config/nvim.bak
+git clone https://www.github.com/0x4248/4248vim ~/.config/nvim
+nvim
+```
+
+After running `nvim`, lazy.vim will automatically install all plugins.
+
+## Licence
+
+This project is licenced under the Apache 2.0 licence. See [LICENCE](LICENCE) for more information.
---
+++ nvim/lua/config/lazy.lua
@@ -0,0 +1,104 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+require("lazy").setup({
+  spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    {
+    "tokyonight.nvim",
+        opts = {
+        transparent = true,
+        styles = {
+           sidebars = "transparent",
+           floats = "transparent",
+        },
+    }
+    },
+    { 'wakatime/vim-wakatime', lazy = false },
+    {
+      "nvimdev/dashboard-nvim",
+      event = "VimEnter",
+      opts = function()
+        local logo = [[
+██╗  ██╗██████╗ ██╗  ██╗ █████╗ ██╗   ██╗██╗███╗   ███╗
+██║  ██║╚════██╗██║  ██║██╔══██╗██║   ██║██║████╗ ████║
+███████║ █████╔╝███████║╚█████╔╝██║   ██║██║██╔████╔██║
+╚════██║██╔═══╝ ╚════██║██╔══██╗╚██╗ ██╔╝██║██║╚██╔╝██║
+      ██║███████╗     ██║╚█████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+      ╚═╝╚══════╝     ╚═╝ ╚════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]]
+
+        logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+        local opts = {
+          theme = "doom",
+          hide = {
+            statusline = false,
+          },
+          config = {
+            header = vim.split(logo, "\n"),
+        center = {
+          { action = LazyVim.telescope("files"),                                    desc = " Find File",       icon = " ", key = "f" },
+          { action = "ene | startinsert",                                        desc = " New File",        icon = " ", key = "n" },
+          { action = "Telescope oldfiles",                                       desc = " Recent Files",    icon = " ", key = "r" },
+          { action = "Telescope live_grep",                                      desc = " Find Text",       icon = " ", key = "g" },
+          { action = "Neotree",                                                 desc = " File Explorer",   icon = " ", key = "e" },
+          { action = [[lua LazyVim.telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
+          { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
+          { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
+          { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
+          { action = "LazyVim.terminal()",                                       desc = " Terminal",        icon = " ", key = "t" },
+          { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
+        },
+            footer = function()
+              local stats = require("lazy").stats()
+              local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+              return { "⚡ neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms ⚡" }
+            end,
+          },
+        }
+
+        for _, button in ipairs(opts.config.center) do
+          button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+          button.key_format = "  %s"
+        end
+
+        if vim.o.filetype == "lazy" then
+          vim.cmd.close()
+          vim.api.nvim_create_autocmd("User", {
+            pattern = "DashboardLoaded",
+            callback = function()
+              require("lazy").show()
+            end,
+          })
+        end
+
+        return opts
+      end,
+    },
+  },
+
+  defaults = {
+    lazy = false,
+    version = false,
+  },
+
+  checker = { enabled = true },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
+vim.opt.relativenumber = false
+
