fbbc2efa950edce8e9044eb03c7df0ffecc0bd8b
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-08-30 09:09:53 +0100

    Update dotfiles
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,38 @@
+alias python python3
+alias pip pip3
+alias ll 'ls -alF'
+alias la 'ls -A'
+alias l 'ls -CF'
+alias grep 'grep --color=auto'
+alias c clear
+alias cls clear
+alias n nvim
+alias e exit
+alias q exit
+alias quit exit
+
+function fish_greeting
+    set CYAN (set_color -o cyan)
+    set NC (set_color normal)
+    echo "Welcome to $(uname -s) $(uname -r)"
+    echo ""
+    echo "    * Hostname: $CYAN$(uname -n)$NC"
+    echo "    * Uptime: $CYAN$(uptime)$NC"
+    echo "    * Logged in as: $CYAN$(whoami)$NC"
+    echo "    * Shell: $CYAN$SHELL$NC"
+    echo ""
+    echo ---------------------------------
+    echo "Login time:"
+    echo "$CYAN$(date)$NC"
+    echo ---------------------------------
+end
+
+function owoii
+    owo $argv | figlet
+end
+
+set -x PATH $PATH ~/.scripts
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+
 end
